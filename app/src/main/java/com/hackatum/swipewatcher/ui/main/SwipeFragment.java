f5385650b29e55b618dd451e51154d8824414133
Tobias <inzinger.tobias@gmail.com>
2022-11-19 01:16:59 +0100

    first

---
+++ app/src/main/java/com/hackatum/swipewatcher/ui/main/SwipeFragment.java
@@ -0,0 +1,62 @@
+package com.hackatum.swipewatcher.ui.main;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import com.hackatum.swipewatcher.R;
+import com.hackatum.swipewatcher.databinding.FragmentMainBinding;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class SwipeFragment extends Fragment {
+
+    private PageViewModel pageViewModel;
+    private FragmentMainBinding binding;
+
+    public static SwipeFragment newInstance() {
+        SwipeFragment fragment = new SwipeFragment();
+        Bundle bundle = new Bundle();
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        pageViewModel = new ViewModelProvider(this).get(PageViewModel.class);
+        int index = 100;
+        pageViewModel.setIndex(index);
+    }
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+
+        binding = FragmentMainBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.sectionLabel;
+        pageViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
